c0ab822d45173425a2505d38df951c87a95c19d0
Pablokz1 <pablo.carvalho16@hotmail.com>
2025-07-31 17:43:33 -0300

    feat: implements findBalanceByAccountId method in AccountsGateway

--- src/domain/accounts/gateway/acconts.gateway.ts
+++ src/domain/accounts/gateway/acconts.gateway.ts
@@ -6,4 +6,6 @@ export interface AccountsGateway {
     list(): Promise<Accounts[]>;
 
     findById(id: string): Promise<Accounts | null>;
+
+    findBalanceByAccountId(accountId: string): Promise<number | null>;
 }
