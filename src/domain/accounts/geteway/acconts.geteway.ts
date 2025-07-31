ca6e8ef515ef92206ee7fd46838face88e9be7f0
Pablokz1 <pablo.carvalho16@hotmail.com>
2025-07-31 17:00:33 -0300

    feat: create acconts gateway

---
+++ src/domain/accounts/geteway/acconts.geteway.ts
@@ -0,0 +1,9 @@
+import type { Accounts } from "../entity/accounts.entity.js";
+
+export interface AccountsGateway {
+    save(account: Accounts): Promise<void>;
+
+    list(): Promise<Accounts[]>;
+
+    findById(id: string): Promise<Accounts | null>;
+}
