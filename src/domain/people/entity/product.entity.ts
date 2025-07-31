93ff8a7e9e754c3d229e1bd654b7c04d5721e0c0
Pablokz1 <pablo.carvalho16@hotmail.com>
2025-07-31 16:12:10 -0300

    feat: creates entity of people

---
+++ src/domain/people/entity/product.entity.ts
@@ -0,0 +1,41 @@
+export type PeopleProps = {
+    id: string;
+    name: string;
+    document: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export class People {
+    private constructor(private props: PeopleProps) { 
+    }
+
+    public static create(name: string, document: string, password: string) { 
+        return new People({
+            id: crypto.randomUUID(),
+            name,
+            document,
+            password,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        });
+    }
+
+    public static with(props: PeopleProps) {
+        return new People(props);
+    }
+
+    public get id() {
+        return this.props.id;
+    }
+
+    public get name() {
+        return this.props.name;
+    }
+
+    public get document() {
+        return this.props.document;
+    }
+
+}
